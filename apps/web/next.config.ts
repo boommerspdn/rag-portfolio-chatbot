15d5ff5cb5a4843af4e97c5bfae98f626716ed36
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2026-05-02 22:57:13 +0700

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.1.18"],
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
 };
 
 export default nextConfig;
